6e66a83769a67290dc16ad34d722a3f60402c1ae
zupolini <4dm#zup0l1n1>
2019-06-06 10:19:16 -0300

    database

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,17 @@ const knexfile = require('../knexfile')
 const knex = require('knex')(knexfile)
 module.exports = knex
 
-knex('users').insert({
-    name: "Teste",
-    email: "teste@teste.com.br",
-    password: "teste"
-}).then(data => console.log(data))
+// Exemplo de inserção no banco de dados
+
+// knex('users').insert({
+//     name: "Teste",
+//     email: "teste@teste.com.br",
+//     password: "teste"
+// }).then(data => console.log(data))
+
+// Exemplos de consultas
+
+// knex('users').then(resultado => console.log(resultado))
+// knex('users').select("*").then(resultado => console.log(resultado))
+// knex('users').first().then(resultado => console.log(resultado))
+// knex('users').where({id: 1}).first().then(resultado => console.log(resultado))
